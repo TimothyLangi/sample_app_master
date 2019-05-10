cfb6c8d0187278d748012635c33390e46ffc5687
Your Name <your.email@example.com>
2019-05-10 07:42:46 +0000

    finised

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-   test "valid signup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
@@ -12,6 +12,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
